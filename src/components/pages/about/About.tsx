42762d5a2e0cdc0a3aafe01e9a9e0553142400c6
ciscodevelop <ciscopp@gmail.com>
2023-01-26 23:19:23 +0100

    Updates

--- src/components/pages/about/About.tsx
+++
@@ -1,9 +0,0 @@
-import   './about.scss'
-
-function About() {
-  return (
-    <div className='about-container' id='about'>About</div>
-  )
-}
-
-export default About
--- src/components/pages/projects/Projects.tsx
+++
@@ -1,9 +0,0 @@
-import   './projects.scss'
-
-function Projects() {
-  return (
-    <div className='projects-container' id='projects'>Projects</div>
-  )
-}
-
-export default Projects
