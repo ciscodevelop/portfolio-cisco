756757edc984d33771d023fe19ee1fe0954296ca
ciscodevelop <ciscopp@gmail.com>
2023-01-26 21:47:43 +0100

    creato skills

--- src/components/pages/skills/Skills.tsx
+++ src/components/pages/skills/Skills.tsx
@@ -1,23 +1,68 @@
-import   './skills.scss'
+import { useEffect, useState } from "react";
+import "./skills.scss";
+import SkillsList from "./skillsList/SkillsList";
+import { BackEnd, DataBase, Design, FrontEnd } from "../../../data/DataSkills";
+interface skillsProps {
+  id: string;
+  title: string;
+  img: string;
+}
+interface skillsListProps {
+  id: string;
+  title: string;
+}
+interface SkillsData {
+  [key: string]: skillsProps[];
+}
+ 
+function Skills() {   
+  const [SelectedCategory, setSelected] = useState  ("frontend");
+  const [DataSkills, setDataSkills] = useState<skillsProps[]>([]);
+  
+  useEffect(() => {
+    const skillsData:SkillsData = {
+      frontend: FrontEnd,
+      backend: BackEnd,
+      design: Design,
+      dataBase: DataBase
+    }
+    setDataSkills(skillsData[SelectedCategory]);
+    console.log('passed');
+    
+  }, [SelectedCategory]);
 
-function Skills() {
+  const listCategory: skillsListProps[] = [
+    { id: "frontend", title: "Frontend" },
+    { id: "backend", title: "Backend" },
+    { id: "design", title: "Design" },
+    { id: "dataBase", title: "Data Base" },
+  ];
+
   return (
-    <div className='skills-container' id='skills'>
+    <div className="skills-container" id="skills">
       <h1>Skills</h1>
       <ul>
-        <li>Futured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
+        {listCategory.map((item) => (
+          <SkillsList
+            key={item.id}
+            id={item.id}
+            active={SelectedCategory === item.id}
+            title={item.title}
+            setSelected={setSelected}
+          />
+        ))}
       </ul>
       <div className="items-container">
-        <div className="item">
-          <img src="asset/pngegg.png" alt="" />
-          <h3>JavaScript</h3>
-        </div>
+        {DataSkills.map((skill: any) => (          
+          <div className="item" key={skill.id}>
+            <img src={skill.img} alt={skill.title}   />
+            <h3 >{skill.title} </h3>            
+          </div>           
+           
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
---
+++ src/components/pages/skills/skillsList/SkillsList.tsx
@@ -0,0 +1,22 @@
+import { Dispatch } from "react";
+import "./skillsList.scss";
+
+interface Props {
+  id?: string;
+  title: string;
+  active: boolean;
+  setSelected: Dispatch<any>;
+}
+
+const SkillsList: React.FC<Props> = ({ id, title, active, setSelected }) => {
+  return (
+    <li
+      className={active ? "skillsList active" : "skillsList"}
+      onClick={() => setSelected(id)}
+    >
+      {title}
+    </li>
+  );
+};
+
+export default SkillsList;
